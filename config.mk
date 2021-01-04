1119853a7b2d74169d860be43f6a95b313c69f46
monosans <hsyqixco@protonmail.com>
2021-01-04 11:38:25 +0300

    Delete st.1

--- config.mk
+++ config.mk
@@ -5,7 +5,6 @@ VERSION = 0.8.4
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
@@ -29,9 +28,6 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
 
 # compiler and linker
 # CC = c99
